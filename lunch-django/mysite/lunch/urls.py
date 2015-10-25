9631ebae95a93a48bca4f7b6bc5561031c7e65e7
GabbasovDinar <gabbasov@it-projects.info>
2015-10-25 21:06:48 +0500

    Обновление
    25 октября 2015

--- lunch-django/mysite/lunch/urls.py
+++ lunch-django/mysite/lunch/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<OrderProductLine_id>[0-9]+)/$', views.orders, name='orders'),
+    url(r'^(?P<OrderProductLine_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<OrderProductLine_id>[0-9]+)/processing/$', views.processing, name='processing'),
+    url(r'^(?P<user_id>[0-9]+)/reguser/$', views.reguser, name='reguser'),
 ]
--- lunch-django/mysite/lunch/views.py
+++ lunch-django/mysite/lunch/views.py
@@ -1,7 +1,32 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+#from .models import OrderProductLine
+from .models import User, Order, OrderProductLine
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    latest_user_list = User.objects.order_by('-RegistrationDate')[:5]
+    template = loader.get_template('lunch/index.html')
+    context = RequestContext(request, {'latest_user_list': latest_user_list,})   
+    return HttpResponse(template.render(context))
+def cash(request, cash_id):
+    
+def orders(request, OrderProductLine_id):
+    try:
+        orderr = Order.objects.get(pk=OrderProductLine_id)
+    except 
+    return HttpResponse("You're looking at order %s." % OrderProductLine_id)
 
+def results(request, OrderProductLine_id):
+    response = "You're looking at the results of order %s."
+    return HttpResponse(response % OrderProductLine_id)
+
+def processing(request, OrderProductLine_id):
+    return HttpResponse("You're voting on order %s." % OrderProductLine_id)
+
+def reguser(request, user_id):
+    response = "You're looking at the results of user %s."
+    return HttpResponse(response % user_id)    
+#def regprocessing(request, user_id)
+#def login(request, user_id) 
 # Create your views here.
