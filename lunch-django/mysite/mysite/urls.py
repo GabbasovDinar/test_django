b1223bf65b106185fe816faa61cdae66e89e9ea6
Dinar <gabbasov@it-projects.info>
2015-11-05 02:24:03 +0500

    Revert "изменение 9"
    This reverts commit 5c8f3cfdaeef3382669749cc7a7e4d025826f4df.

--- lunch-django/mysite/mysite/urls.py
+++ lunch-django/mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^preg/', include('preg.urls', namespace="preg")),
+    url(r'^lunch/', include('lunch.urls', namespace="lunch")),
     url(r'^admin/', include(admin.site.urls)),
 ]
